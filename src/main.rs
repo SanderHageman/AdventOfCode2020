463d2b14d4e5ad719b3fc117b1f6659e9e22b299
Sander <sander12101@gmail.com>
2020-12-01 20:25:46 +0100

    automatically fetch day input

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
-use std::fs;
+use std::{fs, io::Result};
 
 mod day1;
 
 fn main() {
-    day1::day(get_input(1));
+    day1::day(get_input(1, 2020));
 }
 
-fn get_input(day: usize) -> String {
-    let file_path = format!("input/day{}.txt", day);
-    fs::read_to_string(file_path)
-        .expect("Something went wrong!")
+fn get_input(day: usize, year: usize) -> String {
+    let file_path = format!("input/day{}", day);
+
+    let file_string = fs::read_to_string(file_path.clone());
+
+    let result: String;
+
+    if file_string.is_err() {
+        let online = get_online_input(day, year);
+        if let Ok(online_result) = online {
+            println!("Fetching input for {}/{} online", day, year);
+            fs::write(file_path.clone(), online_result.clone()).expect("Unable to write to cache");
+            result = online_result;
+        } else {
+            result = String::new();
+        }
+    } else {
+        result = file_string.unwrap();
+    }
+
+    result.trim().to_owned()
+}
+
+fn get_online_input(day: usize, year: usize) -> Result<String> {
+    let session_id = fs::read_to_string("input/session_id")
+        .expect("Unable to read session id at input/session_id")
         .trim()
-        .to_owned()
+        .to_owned();
+
+    let response = ureq::get(&format!(
+        "https://adventofcode.com/{}/day/{}/input",
+        year, day
+    ))
+    .set("Cookie", &format!("session={}", session_id))
+    .call();
+
+    response.into_string()
 }
