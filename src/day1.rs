8ff3c573ce6dcb3639b31e9756c81bb62c7ecfcc
Sander <sander12101@gmail.com>
2020-12-01 15:23:11 +0100

    add day 1

---
+++ src/day1.rs
@@ -0,0 +1,4 @@
+pub fn day(input: String) {
+    println!("Day 1 Result1: {:?}", input);
+    println!("Day 1 Result2: {:?}", 0);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 #![allow(non_snake_case)]
+use std::fs;
+
+mod day1;
 
 fn main() {
-    println!("Hello, world!");
+    day1::day(get_input(1));
+}
+
+fn get_input(day: usize) -> String {
+    let file_path = format!("input/day{}.txt", day);
+    fs::read_to_string(file_path)
+        .expect("Something went wrong!")
+        .trim()
+        .to_owned()
 }
