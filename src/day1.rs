52d51a8c9d4680e31100a604b1e9e4510b2ec62d
Sander <sander12101@gmail.com>
2020-12-01 15:36:25 +0100

    day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,32 @@
 pub fn day(input: String) {
-    println!("Day 1 Result1: {:?}", input);
-    println!("Day 1 Result2: {:?}", 0);
+    let mut result1 = 0;
+    let mut result2 = 0;
+
+    let inputNum = input
+        .lines()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    'outer1: for x in inputNum.iter() {
+        for y in inputNum.iter() {
+            if x + y == 2020 {
+                result1 = x * y;
+                break 'outer1;
+            };
+        }
+    }
+
+    'outer2: for x in inputNum.iter() {
+        for y in inputNum.iter() {
+            for z in inputNum.iter() {
+                if x + y + z == 2020 {
+                    result2 = x * y * z;
+                    break 'outer2;
+                };
+            }
+        }
+    }
+
+    println!("Day 1 Result1: {:?}", result1);
+    println!("Day 1 Result2: {:?}", result2);
 }
