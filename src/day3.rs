31cd30748d8963395b6b20e3e25bb68fada971c1
Sander <sander@shageman.nl>
2020-12-03 09:00:57 +0100

    day 3 part 1 and 2

---
+++ src/day3.rs
@@ -0,0 +1,78 @@
+type TParsed = Vec<Vec<bool>>;
+
+pub fn day(input: String) {
+    let parsed_input = parse(&input);
+
+    println!("Day 3 Result1: {:?}", part_1(&parsed_input));
+    println!("Day 3 Result2: {:?}", part_2(&parsed_input));
+}
+
+fn part_1(input: &TParsed) -> i64 {
+    get_trees_for_slope(&(3, 1), input)
+}
+
+fn part_2(input: &TParsed) -> i64 {
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    slopes
+        .iter()
+        .map(|slope| get_trees_for_slope(slope, input))
+        .product()
+}
+
+fn get_trees_for_slope(slope: &(i32, i32), map: &TParsed) -> i64 {
+    let mut pos = slope.clone();
+    let mut trees = 0i64;
+
+    while let Some(is_tree) = is_tree(&pos, map) {
+        pos.0 += slope.0;
+        pos.1 += slope.1;
+        trees += is_tree as i64;
+    }
+
+    trees
+}
+
+fn is_tree(pos: &(i32, i32), map: &TParsed) -> Option<bool> {
+    let map_x = map[0].len() as i32;
+    let map_y = map.len() as i32;
+    let target_x = (pos.0 % map_x) as usize;
+
+    if pos.1 < map_y {
+        Some(map[pos.1 as usize][target_x])
+    } else {
+        None
+    }
+}
+
+fn parse(input: &str) -> TParsed {
+    input
+        .lines()
+        .map(|line| line.chars().map(|x| x == '#').collect())
+        .collect()
+}
+
+#[test]
+fn test_example_1() {
+    let input = parse(EXAMPLE_INPUT);
+    assert_eq!(part_1(&input), 7)
+}
+
+#[test]
+fn test_example_2() {
+    let input = parse(EXAMPLE_INPUT);
+    assert_eq!(part_2(&input), 336)
+}
+
+#[cfg(test)]
+const EXAMPLE_INPUT: &str = "\
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ extern crate lazy_static;
 
 mod day1;
 mod day2;
+mod day3;
 mod util;
 
 fn main() {
     day1::day(util::get_input(1, 2020));
     day2::day(util::get_input(2, 2020));
+    day3::day(util::get_input(3, 2020));
 }
